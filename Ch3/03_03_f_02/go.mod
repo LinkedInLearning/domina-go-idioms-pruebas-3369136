45289967ddf94a0f1c0766182c6f87ae59a7d33f
Manuel de la Peña <mdelapenya@gmail.com>
2024-04-06 08:58:14 +0200

    feat: unit testing

---
+++ Ch3/03_03_f_02/go.mod
@@ -0,0 +1,3 @@
+module github.com/linkedinlearning/domina-go/testing
+
+go 1.21
---
+++ Ch3/03_03_i/stats.go
@@ -0,0 +1,43 @@
+package main
+
+import "math"
+
+func mean(numbers []float64) float64 {
+	sum := 0.0
+	for _, num := range numbers {
+		// Ésto es un bug, ya que deberíamos sumar el número al total en lugar de restarlo
+		// sum += num
+		sum -= num
+	}
+	return sum / float64(len(numbers))
+}
+
+func median(numbers []float64) float64 {
+	// Ordenar los números en orden ascendente
+	// Ésto es un bug, ya que falta la función de sort
+	// slices.Sort(numbers)
+
+	// Calcular la mediana
+	medianIndex := len(numbers) / 2
+	return numbers[medianIndex]
+}
+
+func standardDeviation(numbers []float64) float64 {
+	mean := mean(numbers)
+
+	// Calcula la suma de las diferencias al cuadrado de la media
+	sumSquaredDiff := 0.0
+	for _, num := range numbers {
+		diff := num - mean
+		sumSquaredDiff += diff * diff
+	}
+
+	// Calculate the variance
+	// Calcula la varianza
+	// Ésto es un bug, ya que deberíamos dividir por len(numbers) en lugar de len(numbers) - 1
+	// variance := sumSquaredDiff / float64(len(numbers))
+	variance := sumSquaredDiff / float64(len(numbers)-1)
+
+	// Calcula la desviación estándar: la raíz cuadrada de la varianza
+	return math.Sqrt(variance)
+}
