0ae161ffb323f7774459d9395d73a8895d1f3f56
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-06 12:59:09 +0200

    docs: comments for the counter with mutex

--- Ch2/02_06/counter.go
+++ Ch2/02_06/counter.go
@@ -7,12 +7,18 @@ type Counter struct {
 	mutex sync.Mutex
 }
 
+// Primero bloqueamos el mutex, incrementamos el valor
+// y luego desbloqueamos el mutex en un defer para asegurarnos
+// de que se desbloquee siempre.
 func (c *Counter) Increment() {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 	c.value++
 }
 
+// Al leer el valor, bloqueamos el mutex, leemos el valor
+// y luego desbloqueamos el mutex en un defer para asegurarnos
+// de que se desbloquee siempre.
 func (c *Counter) GetValue() int {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
