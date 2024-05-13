f3126e2542c77a27314c1659c8580396d051eb4a
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-13 11:14:02 +0200

    docs: add comment for decoupled struct

--- Ch1/01_02_f/main.go
+++ Ch1/01_02_f/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/linkedinlearning/domina-go/package-no-cycles/trainer"
 )
 
+// La estructura Player representa un jugador de Pokemon, y en ella almacenamos
+// un entrenador y una lista de pokemons, de manera desacoplada.
 type Player struct {
 	Player   trainer.Trainer
 	Pokemons []pokemon.Pokemon
