0b92a675e1a693c8606ba3c9057faa8c524efff2
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-14 11:12:16 +0200

    docs: document mutex functions

--- Ch2/02_05/mutex.go
+++ Ch2/02_05/mutex.go
@@ -9,9 +9,13 @@ func mutexCounter() {
 
 	for i := 0; i < 10; i++ {
 		go func() {
+			// Incrementamos el contador dentro de una gorotuine
 			counter.Increment()
 		}()
 	}
 
+	// al terminar el bucle for, obtenemos el valor del contador.
+	// Es muy posible que no todas las gorotuinas hayan terminado,
+	// por lo que el valor será diferente cada vez.
 	fmt.Printf("Hemos capturado %d pokemons\n", counter.GetValue())
 }
