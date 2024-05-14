092d22950da6ce2cd936f2ad04847ceab5ab8a9a
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-14 14:37:49 +0200

    chore: count team rocket pokemons

--- Ch2/02_10/drop.go
+++ Ch2/02_10/drop.go
@@ -16,28 +16,34 @@ func teamRocket() {
 	ch := make(chan Pokemon, pokeballs)
 
 	go func() {
+		count := 0
 		// el team rocket se queda a la espera de que aparezca un pokemon
 		for pokemon := range ch {
+			count++
 			fmt.Printf("team rocket: Hemos capturado al %s!\n", pokemon.Name)
 		}
+
+		fmt.Printf("team rocket: Hemos capturado un total de %d pokemons\n", count)
 	}()
 
 	// 200 es un número suficientemente grande en mi máquina para causar
 	// que la goroutine que representa al team rocket no sea capaz de realizar el trabajo
 	// con el tamaño definido para el canal (el número de pokeballs).
 	const pokemons = 200
+	escaped := 0
 	for p := 1; p <= pokemons; p++ {
 		select {
 		case ch <- PokemonAppears():
 			fmt.Printf("Giovanni: ha aparecido un pokemon, van %d\n", p)
 		default:
 			fmt.Printf("Giovanni: el pokemon %d ha escapado :(\n", p)
+			escaped++
 		}
 	}
 
 	// cerramos el canal para que las goroutines puedan terminar
 	close(ch)
-	fmt.Println("Giovanni: entrenamiento terminado")
+	fmt.Printf("Giovanni: entrenamiento terminado. Se han escapado %d pokemons\n", escaped)
 
 	// esperamos un segundo para que el team rocket pueda capturar los pokemons
 	time.Sleep(time.Second)
