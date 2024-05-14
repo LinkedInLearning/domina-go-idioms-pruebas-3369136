f85df9b0bb814a161055579211b56f4c4233aeed
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-14 13:16:47 +0200

    chore: split pooling files

---
+++ Ch2/02_09/pooling_limit.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+// pokemonIncursionLimit: simulamos una batalla de pokemons, en la que unos cuantos entrenadores intentan capturar pokemons.
+// La diferencia con pokemonIncursion es que en este caso, cada entrenador se retira una vez que ha capturado un pokemon.
+// Si no hay pokemons que capturar, el entrenador se retira.
+func pokemonIncursionLimit() {
+	// todos los pokemons a capturar
+	pokemons := []Pokemon{PokemonAppears(), PokemonAppears(), PokemonAppears(), PokemonAppears(), PokemonAppears()}
+
+	// creamos un pool de entrenadores, una por cada CPU
+	g := runtime.GOMAXPROCS(0)
+
+	// el wait group nos va a permitir limitar el número de goroutines que pueden capturar pokemons
+	var wg sync.WaitGroup
+
+	// añadimos el número de goroutines al contador del WaitGroup
+	wg.Add(g)
+
+	ch := make(chan Pokemon, g)
+
+	// para cada entrenador, creamos una goroutine que espera a que un pokemon aparezca en el canal
+	for c := 0; c < g; c++ {
+		go func(trainer int) {
+			// cuando la goroutine termine, al retirarse, decrementamos el contador de WaitGroup
+			defer wg.Done()
+
+			// la goroutine espera a que un pokemon aparezca en el canal
+			for pokemon := range ch {
+				fmt.Printf("entrenador [%d] ha capturado un pokemon. Es un %s!\n", trainer, pokemon.Name)
+			}
+			fmt.Printf("entrenador [%d] se retira\n", trainer)
+		}(c)
+	}
+
+	// señalamos en el canal que un pokemon ha aparecido
+	for _, p := range pokemons {
+		ch <- p
+	}
+
+	// cerramos el canal para que las goroutines puedan terminar
+	close(ch)
+
+	// esperamos a que todas las goroutines terminen
+	wg.Wait()
+
+	fmt.Println("gimnasio: termina la batalla de pokemons con límite de capturas")
+
+	// esperamos un segundo para que las goroutines puedan capturar los pokemons
+	time.Sleep(time.Second)
+}
